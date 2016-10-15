edea0097ccb1642924ffff34e8ce86f90162fb4f
Maik Schreiber <blizzy@blizzy.de>
2016-10-15 16:45:52 +0200

    KSP 1.2.0

--- PreciseNode/Properties/AssemblyInfo.cs
+++ PreciseNode/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.2.3.0")]
+[assembly: AssemblyFileVersion("1.2.4.0")]
 
 [assembly: KSPAssembly("PreciseNode", 1, 2)]
